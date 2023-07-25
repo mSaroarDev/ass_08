7152805c199ec0e15d53863c6ec70b2a453fbb0a
Muhammad Saroar <136387545+mSaroarDev@users.noreply.github.com>
2023-07-25 12:28:45 +0600

    updated

--- src/pages/Income.jsx
+++ src/pages/Income.jsx
@@ -1,69 +1,99 @@
-import { useRef, useState } from 'react';
-import AppNav from '../components/AppNav';
+import { useRef, useState } from "react";
+import AppNav from "../components/AppNav";
 
 const Income = () => {
+    const desc = useRef();
+    const amount = useRef();
+  const [inputArr, setInputArr] = useState([]);
 
-    const desVal=useRef();
-    const amount=useRef();
+  const [inputData, setInputData] = useState({
+    description: desc.value,
+    amount: amount.value,
+  });
 
+  const changehandle = (e) => {
+    setInputData(setInputData);
+  };
 
-    const [text, setText] = useState('');
-    const [item, setItem] = useState([
-        {
-            description: desVal.value,
-            amount: amount.value
-        }
-    ]);
+  const addData = () => {
+    setInputArr(
+        inputArr.push(setInputData)
+        );
+    setInputArr([...inputArr])
+    console.log(inputArr);
+  };
 
+  return (
+    <div>
+      <AppNav />
 
-    return (
-        <div>
-            <AppNav/>
-            
-            <div className="container">
-                <h1 className=' text-2xl font-bold font-fontBn my-6 text-center'>আপনার উপার্জন যুক্ত করুন</h1>
+      <div className="container">
+        <h1 className=" text-2xl font-bold font-fontBn my-6 text-center">
+          আপনার উপার্জন যুক্ত করুন
+        </h1>
 
-                <div className=' grid grid-cols-2 py-8 gap-9'>
-                    <div className=' bg-slate-100 p-6 rounded-lg'>
-                        <h3 className=' py-3 font-fontBn font-bold italic'>উপার্জনের বর্ননা দিন</h3>
-                        <label className=' font-fontBn' htmlFor="description">ঘটনা লিখুন</label><br />
-                        <input ref={desVal} className='font-fontBn px-2 py-1 border-2 rounded border-gray-100 mb-3' type="text" placeholder='বর্ননা লিখুন' /><br />
-                        <label className=' font-fontBn' htmlFor="amount">টাকার পরিমান লিখুন (ইংরেজীতে) </label><br />
-                        <input ref={amount} className='font-fontBn px-2 py-1 border-2 rounded border-gray-100 mb-3' type="text" placeholder='100' /><br />
-                        <button className="font-fontBn btn btn-active btn-neutral px-4 py-1">যুক্ত করুন</button>
-                    </div>
-                    <div>
-                        <ul>
-                            <li className='listItem font-fontBn text-md flex justify-between bg-slate-100 p-4 rounded-lg mb-4'>
-                                <span className='cross'>x</span>
-                                <p><span className='mr-2'><i className="fa-solid fa-angles-right"></i></span>রিয়েক্ট অ্যাপ বানানো বাবদ</p>
-                                <p>10000 টাকা</p>
-                            </li>
-
-                            <li className='listItem font-fontBn text-md flex justify-between bg-slate-100 p-4 rounded-lg mb-4'>
-                                <span className='cross'>x</span>
-                                <p><span className='mr-2'><i className="fa-solid fa-angles-right"></i></span>রিয়েক্ট অ্যাপ বানানো বাবদ</p>
-                                <p>5000 টাকা</p>
-                            </li>
-
-                            <li className='listItem font-fontBn text-md flex justify-between bg-slate-100 p-4 rounded-lg mb-4'>
-                                <span className='cross'>x</span>
-                                <p><span className='mr-2'><i className="fa-solid fa-angles-right"></i></span>রিয়েক্ট অ্যাপ বানানো বাবদ</p>
-                                <p>200 টাকা</p>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-
-                <div className='flex justify-end font-bold text-lg font-fontBn'>
-                    <p className='mr-6'>Total: </p>
-                    <p>15200 টাকা</p>
-                </div>
-
-            </div>
+        <div className="grid grid-cols-2 py-8 gap-9">
+          <div className="bg-slate-100 p-6 rounded-lg">
+            <h3 className=" py-3 font-fontBn font-bold italic">
+              উপার্জনের বর্ননা দিন
+            </h3>
+            <label className="font-fontBn" htmlFor="description">
+              ঘটনা লিখুন
+            </label>
+            <br />
+            <input
+            ref={desc}
+              className="input"
+              type="text"
+              placeholder="বর্ননা লিখুন"
+              name="description"
+              value={inputData.description}
+              onChange={changehandle}
+            />
+            <br />
+            <label className="font-fontBn" htmlFor="amount">
+              টাকার পরিমান লিখুন (ইংরেজীতে){" "}
+            </label>
+            <br />
+            <input
+            ref={amount}
+              className="input"
+              type="text"
+              placeholder="100"
+              name="amount"
+              value={inputData.amount}
+              onChange={changehandle}
+            />
+            <br />
+            <button onClick={addData}>যুক্ত করুন</button>
+          </div>
+          <div>
+            <ul>
+              {inputArr.map((item, ind) => {
+                return (
+                  <li key={ind} className="listItem font-fontBn text-md flex justify-between bg-slate-100 p-4 rounded-lg mb-4">
+                    <span className="cross">x</span>
+                    <p>
+                      <span className="mr-2">
+                        <i className="fa-solid fa-angles-right"></i>
+                      </span>
+                      {item.description}
+                    </p>
+                    <p>{item.amount} টাকা</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-    );
+
+        <div className="flex justify-end font-bold text-lg font-fontBn">
+          <p className="mr-6">Total: </p>
+          <p>15200 টাকা</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Income;
