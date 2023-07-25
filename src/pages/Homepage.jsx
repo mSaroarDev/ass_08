a2140135c32437f6f4ea1d77359146616844bffd
Muhammad Saroar <136387545+mSaroarDev@users.noreply.github.com>
2023-07-25 19:44:16 +0600

    react logo updated

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,11 +8,21 @@ const Homepage = () => {
       <div className="mt-[-80px] h-screen w-full flex items-center justify-center">
         <div>
           <div className="logo-container flex items-center justify-center mb-6">
-            <img
-              className="rotate-infinite w-52"
-              src="./../../src/assets/logo.png"
-              alt="React Logo"
-            />
+            <svg
+              width="100%"
+              height="100%"
+              viewBox="-10.5 -9.45 21 18.9"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class="text-[#61DBFB] rotate-infinite mt-4 mb-3 text-link dark:text-link-dark w-24 lg:w-28 self-center text-sm mr-0 flex origin-center transition-all ease-in-out"
+            >
+              <circle cx="0" cy="0" r="2" fill="currentColor"></circle>
+              <g stroke="currentColor" stroke-width="1" fill="none">
+                <ellipse rx="10" ry="4.5"></ellipse>
+                <ellipse rx="10" ry="4.5" transform="rotate(60)"></ellipse>
+                <ellipse rx="10" ry="4.5" transform="rotate(120)"></ellipse>
+              </g>
+            </svg>
           </div>
           <h1 className=" font-fontBn font-bold text-3xl text-center">
             রিয়েক্ট দিয়ে তৈরী করা সাধারন একটি অ্যাপ। <br /> যা আপনার উপার্জন এবং
